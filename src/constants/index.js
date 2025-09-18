d6ac2dfa50dc374c2052fbfd09aaf0598fd0d943
sanyog2005 <20sanyogsharma.com>
2025-09-18 22:38:26 +0530

    hello

---
+++ src/constants/index.js
@@ -0,0 +1,292 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "key Boost - digital marketing website",
+    description:
+      " website for a digital Marketing agency",
+    subDescription: [
+      "Developed a responsive website using html,js and Tailwind CSS for a digital marketing agency.",
+    ],
+    href: "https://keyboost.netlify.app/",
+    logo: "",
+    image: "/assets/images/pr1.png",
+    tags: [
+       {
+        id: 1,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 2,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Orbit Ways",
+    description:
+      "A hackathon winning project in which I created a website for space exploration company",
+    subDescription: [
+      "Built a responsive website using html and Tailwind CSS for a space exploration company.",
+    ],
+    href: "https://sanyog2005.github.io/orbitways/",
+    logo: "",
+    image: "/assets/images/pr2.png",
+    tags: [
+       {
+        id: 1,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 2,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    
+    ],
+  },
+  {
+    id: 3,
+    title: "Park king",
+    description:
+      "An website that helps to find a desired parking spot for your vehicle",
+    subDescription: [
+      "Developed a responsive website using MERN stack and Tailwind CSS for finding parking spots.",
+    ],
+    href: "https://github.com/sanyog2005/code-streak-android-app",
+    logo: "",
+    image: "/assets/images/pr8.jpeg",
+    tags: [
+      {
+        id: 1,
+        name: "git",
+        path: "/assets/logos/git.svg",
+      },
+      {
+        id: 2,
+        name: "JavaScript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 3,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  // {
+  //   id: 4,
+  //   title: "C++ Game Engine",
+  //   description:
+  //     "A lightweight C++ game engine designed for 2D and 3D game development.",
+  //   subDescription: [
+  //     "Built a powerful rendering engine using OpenGL and C++.",
+  //     "Developed a physics engine with collision detection and particle effects.",
+  //     "Implemented a scripting system for easy game customization.",
+  //     "Optimized performance with multi-threading and efficient memory management.",
+  //   ],
+  //   href: "",
+  //   logo: "",
+  //   image: "/assets/projects/game-engine.jpg",
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "C++",
+  //       path: "/assets/logos/cplusplus.svg",
+  //     },
+  //     {
+  //       id: 2,
+  //       name: "C#",
+  //       path: "/assets/logos/csharp.svg",
+  //     },
+  //     {
+  //       id: 3,
+  //       name: "Git",
+  //       path: "/assets/logos/git.svg",
+  //     },
+  //     {
+  //       id: 4,
+  //       name: "Microsoft",
+  //       path: "/assets/logos/microsoft.svg",
+  //     },
+  //   ],
+  // },
+  {
+    id: 5,
+    title: "E- Commerce Website",
+    description:
+      "A fully functional e-commerce website built with WordPress and Tailwind CSS.",
+    subDescription: [
+      "Developed a responsive e-commerce website using WordPress and Tailwind CSS.",
+      "Integrated payment gateways and shopping cart functionality.",
+      "Customized themes and plugins to enhance user experience.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/accessories.jpg",
+     tags: [
+      {
+        id: 1,
+        name: "git",
+        path: "/assets/logos/git.svg",
+      },
+      {
+        id: 2,
+        name: "JavaScript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 3,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Online Learning Platform",
+    description:
+      "A web application that allows users to enroll in courses, watch video lectures, and take quizzes.",
+    subDescription: [
+      "Built a full-stack web application using React for the frontend and Node.js with Express for the backend.",],
+    href: "",
+    logo: "",
+    image: "/assets/projects/elearning.jpg",
+     tags: [
+      {
+        id: 1,
+        name: "git",
+        path: "/assets/logos/git.svg",
+      },
+      {
+        id: 2,
+        name: "JavaScript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 3,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "LeetCode",
+    href: "",
+    icon: "/assets/logos/OIP.jpg",
+    
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/sanyogsharma/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "GitHub",
+    href: "https://github.com/sanyog2005",
+    icon: "/assets/logos/github.svg",
+    
+  },
+];
+
+export const experiences = [
+    {
+    title: "Web Dev Intern",
+    job: "HiSync , Ghaziabad",
+    date: "1 July to 31 July 2025",
+    contents: [
+     " worked on a library management system"
+    ],
+  },
+  {
+    title: "Android Developer Intern",
+    job: "Poxo RFID Automation-Janakpuri east, Delhi",
+    date: "3 July to 8 Aug 2024"
+,
+    contents: [
+        "Worked as an android Development intern and worked on a cafe management app"
+    ]   
+      },
+
+  {
+    title: "B-TECH",
+    job: "ARTIFICIAL INTELLIGENCE AND DATA SCIENCE",
+    date: "2022-2026",
+    contents: [
+      "VIVEKANANDA INSTITUTE OF PROFESSIONAL STUDIES TECHNICAL",
+      "PURSUING (YEAR OF COMPLETION:EXPECTED 2026)",
+    ],
+  },
+];
+export const reviews = [
+  {
+    name: "Hackathon",
+    username: "Runner up at Nebula Net [MAIT]",
+    body: " Our project: Cosmos theme & the problem statement were given by the organizers 🚀",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Hackathon",
+    username: "Finalist VIHAAN 007 [DTU]",
+    body: "Exhilarating journey at the Vihaan007 hackathon hosted by IEEE DTU on April 12-13! 🌟 We're thrilled to share that we not only participated but also successfully",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "Hackathon",
+    username: "Finalist HACKAZARDS [BPIT]",
+    body: "During the hackathon, we dedicated ourselves to crafting a groundbreaking solution: Smart and Effective Real-Time Management of Parking",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Hackathon",
+    username: "Participant at HACK THE CRISIS [NSIT]",
+    body: "While hackathons can be challenging, they're an incredible way to boost productivity and focus. ",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Open Source Contributor",
+    username: "SOCIAL SUMMER OF CODE 2024",
+    body: "Being part of this incredible journey, contributing to meaningful projects, and connecting with a global community of developers has been nothing short of inspiring.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Hackathon",
+    username: "Code4Cause 2024 [NSUT]",
+    body: "wrapped up an unforgettable journey at Code4Cause 2024, a 30-hour hackathon organized by HackwithIndia at Netaji Subhas Institute of Technology",
+    img: "https://robohash.org/charlie",
+  },
+ 
+];
