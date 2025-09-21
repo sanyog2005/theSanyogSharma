36e6cbacc7babe0a35abcd08f7d6b4fd5c60a41a
SANYOG SHARMA <142544426+sanyog2005@users.noreply.github.com>
2025-09-21 11:14:14 +0530

    Update HeroText.jsx

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -61,7 +61,7 @@ const HeroText = () => {
           animate="visible"
           transition={{ delay: 1 }}
         >
-          Hi,I'm Ali
+          Hi,I'm Sanyog
         </motion.p>
         <div>
           <motion.p
