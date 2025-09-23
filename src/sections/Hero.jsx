a81368aa14b4227b8347eef85290d5d1b50bd6cd
SANYOG SHARMA <142544426+sanyog2005@users.noreply.github.com>
2025-09-23 21:59:50 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -21,10 +21,10 @@ const Hero = () => {
         <Canvas camera={{ position: [0, 1, 3] }}>
           <Suspense fallback={<Loader />}>
             <Float>
-              <Astronaut
+              {/* <Astronaut
                 scale={isMobile && 0.23}
                 position={isMobile && [0, -1.5, 0]}
-              />
+              /> */}
             </Float>
             <Rig />
           </Suspense>
